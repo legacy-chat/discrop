f7a82e175e99467dd9a8ab5981ad42e471d2b5f6
Meep3692 <meep3692@gmail.com>
2023-10-27 02:04:20 -0400

    Message #1

---
+++ src/main/java/ca/awoo/discrop/resources/channel/Message.java
@@ -0,0 +1,41 @@
+package ca.awoo.discrop.resources.channel;
+
+import java.util.Date;
+
+import ca.awoo.discrop.Snowflake;
+import ca.awoo.discrop.resources.user.User;
+import ca.awoo.fwoabl.Optional;
+
+public class Message {
+    private Snowflake id;
+    private Snowflake channel_id;
+    private User author;
+    private String content;
+    private Date timestamp;
+    private Date edited_timestamp;
+    private boolean tts;
+    private boolean mention_everyone;
+    private User[] mentions;
+    private Role[] mention_roles;
+    private Optional<ChannelMention[]> mention_channels;
+    private Attachment[] attachments;
+    private Embed[] embeds;
+    private Optional<Reaction[]> reactions;
+    private Optional<String> nonce; //TODO: int or string because of the weird way discord handles bigint
+    private boolean pinned;
+    private Optional<Snowflake> webhook_id;
+    private int type;
+    private Optional<MessageActivity> activity;
+    private Optional<Application> application;
+    private Optional<Snowflake> application_id;
+    private Optional<MessageReference> message_reference;
+    private Optional<Integer> flags;
+    private Optional<Message> referenced_message;
+    private Optional<MessageInteraction> interaction;
+    private Optional<Channel> thread;
+    //TODO: components
+    private Optional<StickerItem[]> sticker_items;
+    private Optional<Integer> position;
+    //TODO: role_subscription_data
+    //TODO: resolved
+}
