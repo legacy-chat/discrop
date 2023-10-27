956ef601fd2708e324a49926af948258d7d1d810
Meep3692 <meep3692@gmail.com>
2023-10-27 02:17:18 -0400

    ChannelMention #1

---
+++ src/main/java/ca/awoo/discrop/resources/channel/ChannelMention.java
@@ -0,0 +1,10 @@
+package ca.awoo.discrop.resources.channel;
+
+import ca.awoo.discrop.Snowflake;
+
+public class ChannelMention {
+    private Snowflake id;
+    private Snowflake guild_id;
+    private int type;
+    private String name;
+}
