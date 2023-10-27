d180206bdb0ef2353d22213f6c14b9e7003134b7
Meep3692 <meep3692@gmail.com>
2023-10-27 02:09:35 -0400

    GuildMember #1

--- src/main/java/ca/awoo/discrop/resources/channel/ThreadMember.java
+++ src/main/java/ca/awoo/discrop/resources/channel/ThreadMember.java
@@ -3,6 +3,7 @@ package ca.awoo.discrop.resources.channel;
 import java.util.Date;
 
 import ca.awoo.discrop.Snowflake;
+import ca.awoo.discrop.resources.guild.GuildMember;
 import ca.awoo.fwoabl.Optional;
 
 public class ThreadMember {
--- src/main/java/ca/awoo/discrop/resources/guild/GuildMember.java
+++ src/main/java/ca/awoo/discrop/resources/guild/GuildMember.java
@@ -1,5 +1,22 @@
 package ca.awoo.discrop.resources.guild;
 
+import java.util.Date;
+
+import ca.awoo.discrop.Snowflake;
+import ca.awoo.discrop.resources.user.User;
+import ca.awoo.fwoabl.Optional;
+
 public class GuildMember {
-    
+    private Optional<User> user;
+    private Optional<String> nick;
+    private Optional<String> avatar;
+    private Snowflake[] roles;
+    private Date joined_at;
+    private Optional<Date> premium_since;
+    private boolean deaf;
+    private boolean mute;
+    private int flags;
+    private Optional<Boolean> pending;
+    private Optional<String> permissions;
+    private Optional<Date> communication_disabled_until;
 }
